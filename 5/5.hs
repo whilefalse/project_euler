33ae387182b8429b6ba5a45333932a718fdcebf1
Steven Anderson <steve@whilefalse.net>
2012-07-10 22:00:39 +0100

    number 5 in haskell:

---
+++ 5/5.hs
@@ -0,0 +1,18 @@
+-- Start at product [1..20]
+-- Try divisors up to 20 while the result is still acceptable
+nums :: (Integral a) => [a]
+nums = [1..20]
+
+divisible :: (Integral a) => a -> Bool
+divisible n = null (filter (\p -> n `mod` p /= 0) nums)
+
+best :: (Integral a) => a -> a -> a
+best start factor
+  | factor > last nums    = start
+  | divisible next        = best next factor
+  | otherwise             = best start (factor + 1)
+  where
+  next  = start `div` factor
+
+answer :: (Integral a) => a
+answer = best (product nums) 2
