c2721534b89396a4ccecb9cf23ce57d0a7d91bc3
Steven Anderson <steve@whilefalse.net>
2012-07-10 22:39:00 +0100

    Foldr FTW

--- 5/5.hs
+++ 5/5.hs
@@ -1,22 +1,5 @@
--- Start at product [1..20]
--- Try divisors up to 20 while the result is still acceptable
-nums :: (Integral a) => [a]
-nums = [1..20]
+divisible n = sum (map (mod n) [1..20]) == 0
 
-divisible :: (Integral a) => a -> Bool
-divisible n = null (filter (\p -> n `mod` p /= 0) nums)
+least factor acc = if divisible next then next else acc where next = acc `div` factor
 
-best :: (Integral a) => a -> a -> a
-best start factor
-  | factor > last nums    = start
-  | divisible next        = best next factor
-  | otherwise             = best start (factor + 1)
-  where
-  next  = start `div` factor
-
-answer :: (Integral a) => a
-answer = best (product nums) 2
-
--- OR (Awesomeness)
-answer2 :: (Integral a) => a
-answer2 = foldr lcm 1 [1..20]
+answer = foldr least (product [1..20]) [1..20]
