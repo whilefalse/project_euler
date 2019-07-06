30942a1a341efd111efc819b9d7afdd2a8e5315f
Steven Anderson <steve@whilefalse.net>
2019-07-06 22:15:43 +0100

    Some improvements to Sieve of Eratosthenes.

--- 10/10.exs
+++ 10/10.exs
@@ -1,26 +1,24 @@
 defmodule Primes do
   def below(max) do
-    sieve = 2..max |> Enum.map(fn x -> {x, false} end) |> Map.new
+    sieve = 2..max |> Enum.map(fn x -> {x, true} end) |> Map.new
 
-    sieve_all(sieve, 2, max)
-    |> Enum.filter(fn {_, x} -> !x end)
+    2..max
+    |> Enum.reduce(sieve, fn x, sieve -> sieve_all(sieve, x, max) end)
+    |> Enum.filter(fn {_, x} -> x end)
     |> Enum.map(fn {x, _} -> x end)
   end
 
   def sieve_all(sieve, start, max) do
-    cond do
-      start > max -> sieve
-      Map.get(sieve, start) -> sieve_all(sieve, start + 1, max)
-      true ->
-        new_sieve = sieve_factor(sieve, start, start * 2, max)
-        sieve_all(new_sieve, start + 1, max)
+    case Map.get(sieve, start) do
+      false -> sieve
+      true -> sieve_factor(sieve, start, start * start, max)
     end
   end
 
   def sieve_factor(sieve, factor, to_check, max) do
-    cond do
-      to_check > max -> sieve
-      true -> sieve_factor(Map.put(sieve, to_check, true), factor, to_check + factor, max)
+    case to_check > max do
+      true -> sieve
+      false -> sieve_factor(Map.put(sieve, to_check, false), factor, to_check + factor, max)
     end
   end
 end
